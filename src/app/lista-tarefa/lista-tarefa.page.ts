9714cedacb08bef0aa6584af484c913c8dcc6c99
Aluno <noemys379@gmail.com>
2019-06-19 15:41:58 -0300

    recuperacao

---
+++ src/app/lista-tarefa/lista-tarefa.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-tarefa',
+  templateUrl: './lista-tarefa.page.html',
+  styleUrls: ['./lista-tarefa.page.scss'],
+})
+export class ListaTarefaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
